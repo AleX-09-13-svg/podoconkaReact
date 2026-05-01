3b1fb85d078d7e4487855a931d59dceb42275e2d
Alex <designwr@mail.ru>
2026-05-01 18:30:48 +0300

    5

--- src/components/01_homePage/components/HomePageHeader.tsx
+++ src/components/01_homePage/components/HomePageHeader.tsx
@@ -1,12 +1,7 @@
-import { useNavigate } from 'react-router-dom'
-
-import { appPagePathByPage } from '../../../config/appPageRoutes'
 import ContactBar from '../../ui/ContactBar'
 import LandingActionButton from '../../ui/LandingActionButton'
 
 export default function HomePageHeader() {
-  const navigate = useNavigate()
-
   return (
     <div className="col-[1/5] row-[1/3] flex h-full flex-col px-2">
       <div className="mt-3 w-full">
@@ -17,8 +12,10 @@ export default function HomePageHeader() {
       <div className="mt-auto self-center px-4">
         <LandingActionButton
           className="w-full"
-          textForButton="Создать заказ"
-          onClick={() => navigate(appPagePathByPage.calculator)}
+          textForButton="Позвонить"
+          onClick={() => {
+            window.location.href = 'tel:+79647735543'
+          }}
         />
       </div>
     </div>
