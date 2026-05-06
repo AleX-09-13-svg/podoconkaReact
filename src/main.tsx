3181f4ba6c19cedb08dbf4ae7b15ab9e56542f28
Alex <designwr@mail.ru>
2026-05-06 23:01:39 +0300

    14

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react' // рекомендуемая обертка, которая помогает находить потенциальные проблемы в React-приложении.
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom' // Для маршрутизации
 import './index.css'
 import App from './App.tsx'
+ // createRoot() — это функция, которая принимает HTML-элемент и возвращает объект React-корня.
+ //А уже у этого объекта есть метод .render(). 
 
+ 
 createRoot(
   document.getElementById('root')!,
 ).render(
